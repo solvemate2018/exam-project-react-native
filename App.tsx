1289a079a8561c40c1e591d0637e1d29cc2fdf2a
solvemate2018 <35694266+solvemate2018@users.noreply.github.com>
2022-05-31 12:22:38 +0200

    Revert "Initial commit"
    This reverts commit 2042f11cd4bd3e535f1ad51d8b4c2c7c4bc62be5.

--- App.tsx
+++ App.tsx
@@ -1,32 +1,14 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import userReducer from './app/stores/reducers/user.reducers';
-import { StackParamList } from './app/typings/navigations';
-import ReduxThunk from 'redux-thunk';
-import { Provider, useSelector } from 'react-redux';
-import Main from "./app/Main";
-import eventReducer from './app/stores/reducers/event.reducers';
-
-const rootReducer = combineReducers({
-  user: userReducer,
-  event: eventReducer
-});
-
-const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
-
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Main />
-    </Provider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
-
-export type RootState = ReturnType<typeof rootReducer>
 
 const styles = StyleSheet.create({
   container: {
--- app/typings/navigations.ts
+++
@@ -1,7 +0,0 @@
-export type StackParamList = {
-    Welcome: undefined;
-    Signup: undefined;
-    Login: undefined;
-    Profile: undefined;
-    EditProfile: undefined;
-}
